93003dd5bf7bbb95e043787d86c8428e705d279a
Bidyut <deepghosh369@gmail.com>
2024-12-25 21:08:35 +0530

    Add Database URL

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 import express from "express";
 import colors from "colors";
+import dotenv from "dotenv";
+
+//configure env
+dotenv.config();
 
 //rest object
 const app = express();
@@ -10,9 +14,12 @@ app.get("/", (req, res) => {
 });
 
 //port
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 //run listen
 app.listen(PORT, () => {
-  console.log(`server is running on port ${PORT}`.bgGreen.white);
+  console.log(
+    `Server is Running on ${process.env.DEV_MODE} mode on PORT ${PORT}`.bgGreen
+      .white
+  );
 });
